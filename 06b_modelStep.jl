44c66e5fcc267bcbc7b568e5f25ddfe9e134ebc7
radiciandrea <andrea.radici93@libero.it>
2026-08-20 15:03:52 +0200

    added pupae

--- 06b_modelStep.jl
+++ 06b_modelStep.jl
@@ -20,10 +20,14 @@ function  model_step!(model)
 
     #hydrological functioning (CORRECT UNIT OF MEASURES)
     Vprec = model.V
-    model.V = max(0, min(Vprec + model.rho - model.eta, model.Vmax)) 
+    model.V = max(0, min(Vprec + model.mu*(model.rho[model.t] - model.eta[model.t]), model.Vmax)) 
 
     # eggs parameters
+
+    # development
     model.deltaE = max(-0.0008256*tempH^2 + 0.0334072*tempH - 0.0557825, 0.01)*model.dt # egg temperature reaction norm (development)
+    
+    # mortality
     model.muEdt = -log(12.217/(6.115*sqrt(2*pi))*exp(-0.5*(tempH - 24.672)^2/6.115^2))*model.dt # egg mortality rate
     model.muEdt = tempH > -12 ? 0.01*model.dt : 0.1*model.dt # diapausing egg mortality rate
 
@@ -33,6 +37,8 @@ function  model_step!(model)
     #Hatching out of quiescent class
     model.hQ = model.V > Vprec ? model.V/model.Vmax : 0
 
+    #larval parameters
+
     #Primary production of the larval habitat
     model.F = (10^-6 * log10(0.45 + 0.095*tempH)*model.V + model.fd)*model.dt
 
@@ -41,6 +47,48 @@ function  model_step!(model)
 
     # Food available per larvae
     model.alpha = model.F/L
+
+    # development (from GLM)
+    model.deltaLdt = x
+
+    # mortality (from GLM)
+    if model.V > 0
+      if (model.V < model.Vmax) && (model.rho*model.mu < 0.5 * model.Vmax)
+        model.muLdt =x
+      else
+         model.muLdt =x
+      end
+    else
+      model.muLdt =x
+    end
+
+    # Pupae parameters
+    
+    #development
+    model.deltaPdt = -log(max(2.916*10^(-5)*tempH*(tempH - 10.08)*(47.68 - tempH)^(1/0.8317), 0.01))*model.dt
+
+    # mortality 
+    if model.V > 0
+      if (model.V < model.Vmax) && (model.rho*model.mu < 0.5 * model.Vmax)
+        model.muPdt = -log(max(-0.0070628*tempH^2 + 0.3331028*tempH - 2.9878761, 0.01))*model.dt
+      else
+         model.muPdt = model.muDD*model.dt
+      end
+    else
+      model.muPdt = model.muDF*model.dt
+    end
+
+    # Adult parameters
+
+    # size (wing lenght)
+
+    # goniotrophic cycle (equivalent of duration)
+
+    # mortality
+
+    # fecundity
+
+    
     
 
     #generation of new large agents four times a day
