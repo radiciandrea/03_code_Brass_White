90a0d966925bd65e20b134b5fecde2bc5609b940
radiciandrea <andrea.radici93@libero.it>
2026-08-28 14:05:46 +0200

    advanced model Anaylis (pass to R to implement)

--- 09_modelAnalysis.jl
+++ 09_modelAnalysis.jl
@@ -5,40 +5,73 @@ y = 2025
 varParams = funVarParams(y, site, fixedParams);
 steps = varParams.durSim*stepsPerDay
 
-
 scannedParams = Dict(
-    :n_immatureMosquitoes => collect(round.(Int, n_immatureMosquitoes .*(0.8:0.05:1.2))),
-    :seed => collect(1:5),
+    :n_immatureMosquitoes => collect(round.(Int, n_immatureMosquitoes .*(0.95, 1, 1.05))), #collect(round.(Int, n_immatureMosquitoes .*(0.9:0.05:1.1))),
+    :seed => collect(1:5), #collect(1:5)
 
     :n_adultMosquito => 0,
     :dims => (20,20),
-    :dbm => 0,
+
     :varParams => varParams,
-    :dt => fixedParams.dt,
-    :wlmin => fixedParams.wlmin,
-    :wlmax => fixedParams.wlmax,
-    :deltaE => fixedParams.deltaE,
+    #:fixedParams => fixedParams,
+
+    :dbm => 0,
+    :tempH => 0.0,
+    :GPP => 0.0,
+    :alpha => 0.0,
+    :deltaEdt => 0.0,
     :muEdt => 0.0,
-    :muJtotdt => 0.0,
-    :deltaJdt => 0.0,
-    :deltaIdt => 0.0,
-    :deltaGdt => 0.0,
+    :muDEdt => 0.0,
+    :Q => 0.0,
+    :hQ => 0.0,
+    :deltaLdt => 0.0,
+    :muLdt => 0.0,
+
+    :deltaPdt => 0.0,
+    :muPdt => 0.0,
+    :deltaGdt=> 0.0,           
+    :t => 1,
+    :it => 1,
+    :tempH => 0.0,
     :eggs_to_larvae => 0,
     :deggs_to_larvae => 0,
+    :deggs_to_qeggs => 0,
+    :eggs_to_qeggs => 0,
+    :qeggs_to_larvae => 0,
     :laidE => 0,
     :laidED => 0,
     :laidEf => 0,
-    :laidEDf => 0,
-    :totJ => 0,
-    :t => 1,
-    :it => 1
-    )
+    :laidEDf => 0)
 
+    #=
+:V => (fixedParams.sigma*fixedParams.lambda/2), # Brass/White assumed it is completely full at the beginning
+:Vmax => fixedParams.sigma*fixedParams.lambda,
+:sigma => fixedParams.sigma,
+:wlmax => fixedParams.wlmin,
+:wlmin => fixedParams.wlmax,
+:dt => fixedParams.dt,
+:fd => fixedParams.fd,
+:K => fixedParams.K,   
+:muDFdt => (fixedParams.muDF*fixedParams.dt),
+:muDDdt => (fixedParams.muDD*fixedParams.dt)
+=#
+    
 @time spadf, spmdf = paramscan(scannedParams, initialize; adata, mdata, n = steps)
+
+# Pass to R
+CSV.write(string("sim/spadf_", site, "_", y, "_sp.csv"), spadf)
+CSV.write(string("sim/spmdf_", site, "_", y, "_sp.csv"), spmdf)
 
 spamdf = [spadf spmdf[:, [:O]]]
 
 #https://blog.tidy-intelligence.com/posts/dplyr-vs-tidierdata/
+
+sspmdf = @chain spmdf begin
+    @mutate(idRep = string(seed, "_", "n_immatureMosquitoes"))
+    @group_by(idRep)
+    @mutate(O1 = stepsPerDay*spdMean(O, stepsPerDay))
+    @ungroup
+end
 
 sspmdf = @chain spmdf begin
     @group_by(time)
@@ -68,7 +101,7 @@ end
 
 Plots.plot(wsspmdf.O_50,
 ribbon = (wsspmdf.O_50 .- wsspmdf.O_25, wsspmdf.O_75 .- wsspmdf.O_50),
-fillalpha = 0.3);
+fillalpha = 0.3)
 
 CSV.write(string("sim/Sim_", site, "_", y, "_sp.csv"), wsspmdf)
 
