be9fe3bd79a2ce31df9bc916b8364611269462aa
radiciandrea <andrea.radici93@libero.it>
2026-09-02 16:04:42 +0200

    chnages in plots

--- 07_plotDataSettings.jl
+++ 07_plotDataSettings.jl
@@ -5,19 +5,16 @@ Eq(m) = m isa immatureMosquito && m.stage == 2 ? sum(m.abundance) : 0
 L(m) = m isa immatureMosquito && m.stage == 3 ? sum(m.abundance) : 0
 P(m) = m isa immatureMosquito && m.stage == 4 ? sum(m.abundance) : 0
 A(m) = m isa adultMosquito ? 1 : 0
+#VR(m) = m isa breedingSite && m.type == 1 ? m.V : 0
+#VH(m) = m isa breedingSite && m.type == 2 ? m.V : 0
 
 adata = [(Ed, sum), (E, sum), (Eq, sum), (L, sum), (P, sum), (A, sum)]
 
 # Model data save
-O(model) = model.laidE + model.laidED
-V(model) = model.V
+O(model) = model.laidEf + model.laidEDf
+deltaE(model) = model.deltaEdt/model.dt
+muE(model) = model.muEdt/model.dt
+VR(model) = model[3].V
+VH(model) = model[4].V
 
-#mdata = [O, V]
-
-Q(model) = model.Q
-hQ(model) = model.hQ
-alpha(model) = model.alpha
-deltaL(model) = model.deltaLdt/model.dt
-muL(model) = model.muLdt/model.dt
-
-mdata = [O, V, Q, hQ, alpha, deltaL, muL]
+mdata = [O, deltaE, muE, VR, VH]
